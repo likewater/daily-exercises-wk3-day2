20950ed88a0effb52957d99051aabdd401ffa82e
Blake Womack and Grace Stuart <grace@>
2017-02-21 13:29:19 -0800

    add quotes around inputstring id.

--- js/wordPuzzle.js
+++ js/wordPuzzle.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
 
   $(button).submit(function() {
-
+    var inputString = "#stringInput";
     event.preventDefault();
   });//closes btn.submit
 
 }); //closes doc.ready
+
+//
+// Word Puzzle
+// Create a website that uses a method to replace vowels in a string with the dash symbol "-" to create a word puzzle to solve. Hide the original string when you show the resulting word puzzle, so that another person can try and guess it.
+//
+// Example:
+//
+// String Input: "Believe you can and you're halfway there. Theodore Roosevelt"
+//
+// Puzzle Output: "B-l--v- y-- c-n -nd y--'r- h-lfw-y th-r-. Th--d-r- R--s-v-lt"
+//
+// Do this without "regular expressions".
