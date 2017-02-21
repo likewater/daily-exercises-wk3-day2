11e4fc10442313c41c198dec637d4ee684702a0f
Blake Womack and Grace Stuart <grace@>
2017-02-21 08:52:47 -0800

    add initial files.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Create a program that takes two numbers - one to count to and another to determine what multiple to use to get there.
+//
+// Here is some sample input:
+// Count to: 30
+// Count by: 5
+// Output: 5, 10, 15, 20, 25, 30
+//
+// for(index = 5; index <= 30; index += 5) {
+//   alert(index);
+// }
