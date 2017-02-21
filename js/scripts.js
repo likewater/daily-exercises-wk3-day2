6238f42bfca94608415df5e86834826a52b1dbb1
Blake Womack and Grace Stuart <blake@>
2017-02-21 11:56:12 -0800

    add js if else statements and length function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,60 @@
 // Create a program that takes two numbers - one to count to and another to determine what multiple to use to get there.
-//
-// Here is some sample input:
-// Count to: 30
-// Count by: 5
-// Output: 5, 10, 15, 20, 25, 30
-//
-// for(index = 5; index <= 30; index += 5) {
-//   alert(index);
-// }
+  //
+  // Here is some sample input:
+  // Count to: 30
+  // Count by: 5
+  // Output: 5, 10, 15, 20, 25, 30
+  //
+  // for(index = 5; index <= 30; index += 5) {
+  //   alert(index);
+  // }
 
 // Create a program that takes two numbers - one to count to and another to determine what multiple to use to get there.
-//
-// Here is some sample input:
-// Count to: 50
-// Count by: 7
-// Output: 7, 14, 21, 28, 35, 42, 49
+  //
+  // Here is some sample input:
+  // Count to: 50
+  // Count by: 7
+  // Output: 7, 14, 21, 28, 35, 42, 49
 
-// for (index = 7; index <= 50; index += 7) {
-//   alert(index);
-// }
+  // for (index = 7; index <= 50; index += 7) {
+  //   alert(index);
+  // }
+
+// Create a program that takes two numbers - one to count to and another to determine what multiple to use to get there WHEN THE USER JUST PRESSES ENTER WITHOUT ENTERING text
+$(document).ready(function() {
+  var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+  var inputs = ["Blake", "Grace", "bear", "bang", "ran", "boat"];
+  debugger;
+
+
+  if (inputs[0].length === 0) {
+  //validate that all inputs are !null
+    alert("Please enter your answer in all boxes.");
+  } else {
+      alert("testing else");
+      for(var index = 0; index < blanks.length; index += 1) {
+        alert("your word is " + inputs[index] + ".");
+      }
+  }
+  if (inputs === null) {
+    // validate that all inputs are not numbers
+    alert("Please enter non-numberic answers in all fields.");
+  } else {
+    alert("inputs are all alpha char");
+  }
+  // event.preventDefault();
+});
 
 // Go back through the looping practice problems, and redo one of them with a for loop instead of a forEach() loop.
-// > var languages = ['HTML', 'CSS', 'JavaScript'];
-// > languages.forEach(function(language) {
-//     alert('I love ' + language + '!');
-//   });
+  // > var languages = ['HTML', 'CSS', 'JavaScript'];
+  // > languages.forEach(function(language) {
+  //     alert('I love ' + language + '!');
+  //   });
 
-// var languages = ['HTML', 'CSS', 'JavaScript'];
-// for(var index = 0; index < languages.length; index += 1) {
-//   alert("I love " + languages[index] + "!");
-// }
+  // var languages = ['HTML', 'CSS', 'JavaScript'];
+  // for(var index = 0; index < languages.length; index += 1) {
+  //   alert("I love " + languages[index] + "!");
+  // }
 
   // ANOTHER Example:
     // var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
